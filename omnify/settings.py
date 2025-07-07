74416884ca4eb7ed9e30fcc4c1d0d578b39928ff
Yash Ghantala <yashghantala@gmail.com>
2025-07-07 22:33:59 +0530

    Changed authentication class to token based one

--- omnify/settings.py
+++ omnify/settings.py
@@ -126,6 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }

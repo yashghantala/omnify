32a7d01dca535be1907b111dc1ea6f7a27642066
Yash Ghantala <yashghantala@gmail.com>
2025-07-07 23:11:33 +0530

    Added validator for event start and end time

--- event_management/serializers.py
+++ event_management/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework.serializers import ModelSerializer
+from django.utils import timezone
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 from event_management.models import Attendee, Event
 
@@ -7,6 +8,23 @@ class EventSerializer(ModelSerializer):
     class Meta:
         model = Event
         exclude = ["created_by"]
+
+    def validate(self, attrs):
+        """
+        Validates start and end time of a event.
+        """
+
+        start_time = attrs.get("start_time")
+        end_time = attrs.get("end_time")
+        current_time = timezone.now()
+
+        if start_time < current_time:
+            raise ValidationError({"start_time": "Start time must be of future."})
+
+        if end_time < start_time:
+            raise ValidationError({"end_time": "End time must be later to start time."})
+
+        return attrs
 
 
 class AttendeeSerializer(ModelSerializer):
